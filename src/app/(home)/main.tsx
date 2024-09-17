4e4522fb98589b49ead5539b22c074ce7d9e6d11
tiagosan040 <tiagosan040@gmail.com>
2024-09-17 10:30:57 -0300

    feat: implement page of how to play

--- src/app/(home)/main.tsx
+++ src/app/(home)/main.tsx
@@ -32,10 +32,10 @@ export const Main = async () => {
           <Trophy size={28} />
           Classificação
         </Button>
-        <Button className="text-[#8381D9] w-full py-[13px] rounded-xl flex items-center gap-2 font-body text-[18px] bg-transparent border-[2px] border-[#8381D9]">
+        <Link href="/instrucoes" className="tracking-wide font-medium text-[#8381D9] w-full py-[13px] rounded-xl flex items-center justify-center gap-2 font-body text-[18px] bg-transparent border-[2px] border-[#8381D9]">
           <ScrollText size={28} />
           Como Jogar
-        </Button>
+        </Link>
         <Button className="text-yellow-500 w-full py-[13px] rounded-xl flex items-center gap-2 font-body text-[18px] bg-transparent border-[2px] border-yellow-500 hover:text-white hover:bg-yellow-500 transition-all duration-300">
           <Crown size={28} />
           Seja Premium
