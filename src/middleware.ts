0e07e130d0aeafd3c7d25c2770a8779bc9d531e2
tiagosan040 <tiagosan040@gmail.com>
2024-09-24 00:02:46 -0300

    feat: implement middleware to protect authenticated routes

---
+++ src/middleware.ts
@@ -0,0 +1,15 @@
+import { auth } from '@/auth';
+
+export default auth((request) => {
+  const isProtectedRoute = ['/jogar', '/pagamento/processando'].includes(request.nextUrl.pathname);
+  const isUserAutehnticated = !!request.auth;
+
+  if (isProtectedRoute && !isUserAutehnticated) {
+    const url = new URL('/', request.nextUrl.origin);
+    return Response.redirect(url);
+  }
+});
+
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+}
