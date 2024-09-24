fb678a7190320b8048c42dc87f7de49443b4f8af
tiagosan040 <tiagosan040@gmail.com>
2024-09-24 16:41:07 -0300

    feat: integrate clarity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import './globals.css';
+import { Metadata } from 'next';
 import { Roboto } from 'next/font/google';
 import { Toaster } from '@/components/ui/toaster';
 
@@ -8,6 +9,17 @@ const roboto = Roboto({
   variable: '--font-roboto',
 });
 
+export const metadata: Metadata = {
+  title: 'Book Quiz',
+  description: 'Gamifique sua leitura ao participar de quizzes interativos e transforme cada página em um desafio!',
+  openGraph: {
+    title: 'Book Quiz',
+    description: 'Gamifique sua leitura ao participar de quizzes interativos e transforme cada página em um desafio!',
+    url: 'www.bookquiz.com.br',
+    type: 'website',
+  },
+};
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -15,6 +27,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-br">
+      <head>
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "o8dmamqbk9");
+          `,
+          }}
+        />
+      </head>
       <body className={`${roboto.variable} antialiased`}>
         <Toaster />
         {children}
