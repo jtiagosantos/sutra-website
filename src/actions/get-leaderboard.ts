e21fcae1f8932af4e11928e57874db06ffb60cba
tiagosan040 <tiagosan040@gmail.com>
2024-09-24 16:57:49 -0300

    feat: show on leaderboard only users with at least one point

--- src/actions/get-leaderboard.ts
+++ src/actions/get-leaderboard.ts
@@ -5,6 +5,11 @@ import { prisma } from '@/lib/prisma';
 
 export const getLeaderboardAction = actionClient.action(async () => {
   const leaderboard = await prisma.user.findMany({
+    where: {
+      score: {
+        gt: 0,
+      }
+    },
     select: {
       id: true,
       firstName: true,
