d4b90e2b30b076972aef7005c447c787e1784d39
tiagosan040 <tiagosan040@gmail.com>
2024-10-03 22:20:01 -0300

    feat: implement full route to send daily remainder

--- src/app/api/job/daily-reminder/route.tsx
+++ src/app/api/job/daily-reminder/route.tsx
@@ -2,23 +2,41 @@ import { NextResponse } from "next/server";
 import { resend } from "@/lib/resend";
 import Email from '@/email/daily-remainder';
 import { getDailyRemainderTexts } from "@/helpers/get-daily-remainder-texts";
+import { prisma } from "@/lib/prisma";
 
 export const POST = async () => {
   try {
     const { subject, firstText, secondText, thirdText } = getDailyRemainderTexts();
 
-    const { data, error } = await resend.emails.send({
-      from: 'Book Quiz <onboarding@resend.dev>',
-      to: ['tiagosan040@gmail.com'],
-      subject,
-      react: <Email
-        userName="Tiago Santos"
-        quizLink="https://www.bookquiz.com.br/jogar"
-        firstText={firstText}
-        secondText={secondText}
-        thirdText={thirdText}
-      />,
+    const users = await prisma.user.findMany({
+      where: {
+        preferences: {
+          equals: {
+            active_daily_reminder: true,
+          }
+        }
+      },
+      select: {
+        firstName: true,
+        lastName: true,
+        email: true,
+      }
     });
+
+    const { data, error } = await resend.batch.send(
+      users.map((user) => ({
+        from: 'Book Quiz <onboarding@resend.dev>',
+        to: [user.email],
+        subject,
+        react: <Email
+          userName={`${user.firstName} ${user.lastName}`}
+          quizLink="https://www.bookquiz.com.br/jogar"
+          firstText={firstText}
+          secondText={secondText}
+          thirdText={thirdText}
+        />
+      }))
+    );
 
     if (error) {
       return NextResponse.json({ error, message: 'Daily remainder failed' }, { status: 500 })
