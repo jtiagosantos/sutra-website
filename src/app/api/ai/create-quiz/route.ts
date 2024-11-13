d0b167f6f001f5c216f5e9ae428a7ad46c311ca9
tiagosan040 <tiagosan040@gmail.com>
2024-11-13 14:25:09 -0300

    feat: improve prompt that create quiz

--- src/app/api/ai/create-quiz/route.ts
+++ src/app/api/ai/create-quiz/route.ts
@@ -35,32 +35,48 @@ export const POST = async (request: NextRequest) => {
     const { quantityOfQuestions, bookName, authorName } = bodySchema.parse(body);
 
     const prompt = `
-      Eu quero uma lista com exatamente ${quantityOfQuestions} perguntas, nem mais e nem menos, bem detalhadas e profundas sobre o livro "${bookName}", do(a) autor(a) ${authorName}. 
+      ## Função
+        Você é um criador de perguntas, experiente e especializado em criar perguntas sobre livros de maneira bem estruturada, detalhada, profunda e de média/alta dificuldade para responder.
 
-      Me retorne em um json baseado no seguinte schema do zod:
-        z.object({
-          questions: z.array(
-            z.object({
-              title: z.string(),
-              answers: z.array(
-                z.object({
-                  id: z.string(),
-                  text: z.string(),
-                }),
-              ),
-              correct: z.string(),
-            })
-          )
-        })
-      
-      Coisas importantes:
-      - Eu quero que o id de cada answer seja um id do tipo cuid
-      - Eu quero que o campo "correct" seja o id da alternativa correta
-      - É importante que as perguntas sejam bem formuladas e fiéis ao conteúdo do livro
-      - É importante que as respostas façam sentido com o livro
-      - É importante que as perguntas sejam bem detalhadas e profundas
-      - É importante que a lista de perguntas seja variada e não repetitiva
-      - É importante que a lista de perguntas tenha exatamente ${quantityOfQuestions} perguntas
+      ## Input
+        - Quantidade de perguntas a serem criadas
+        - Nome do livro
+        - Nome do autor
+
+      ## Regras de estrutura
+        - O id de cada answer deve ser do tipo cuid (gerado com este pacote npm: https://www.npmjs.com/package/cuid)
+        - O Campo "correct" deve guardar o id da alternativa correta
+        - Cada pergunta deve ter exatamente 4 respostas como alternativas
+        - A lista de perguntas deve ser variada e não repetitiva
+        - A lista de perguntas deve ter exatamente a quantidade de perguntas fornecida como input
+        - Cada pergunta deve ser bem formulada e fiél ao conteúdo do livro
+        - Cada pergunta deve fazer sentido com o livro
+        - Cada pergunta deve ser bem detalhada e profunda
+        - Cada resposta deve ser bem detalhada, bem escrita, bem formulada, bem estruturada e com texto de tamanho médio/grande
+        - Cada resposta deve ser fiél ao conteúdo do livro
+        - Cada resposta deve fazer sentido com o livro
+
+      ## Output
+        - Retornar conjunto de perguntas formatado no schema do zod:
+          z.object({
+            questions: z.array(
+              z.object({
+                title: z.string(),
+                answers: z.array(
+                  z.object({
+                    id: z.string(),
+                    text: z.string(),
+                  }),
+                ),
+                correct: z.string(),
+              })
+            )
+          })
+
+      ## Dados de Input
+        - Quantidade de perguntas a serem criadas: ${quantityOfQuestions}
+        - Nome do livro: ${bookName}
+        - Nome do autor: ${authorName}
     `;
 
     const completion = await openai.chat.completions.create({
